6b4b3c325fc193b0c19c062ccb995c991c8582a6
Jesse Edge <140700684+FredDelaney@users.noreply.github.com>
2026-09-01 18:41:12 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -41,7 +41,7 @@ const nextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+            value: 'camera=(), microphone=(self), geolocation=(), interest-cohort=()',
           },
           {
             key: 'Referrer-Policy',
